b72a01e3a94102e06413f5a90e2a18a32d29d24b
osazuwa matthew ogbebor <osazuwamatthewogbebor@gmail.com>
2025-06-28 16:47:57 +0100

    adjusting landscape mode

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
                     <span className='text-2xl sm:text-3xl cursor-pointer' onClick={toggleMenuIcon}><RiMenu5Fill /></span>
                     <h1 className='text-xl sm:text-[1.8rem] lg:text-3xl sm:mx-11 md:mx-10 lg:mx-0'>RESTAURANT MENU</h1>
                 </nav>
-            <div className={` bg-slate-800 flex flex-col justify-between z-50 absolute top-0 left-0 overflow-y-auto h-screen w-[90%] sm:w-[38%] ${menuIcon? "translate-x-0": "-translate-x-full"} transition-transform duration-1000 ease-in-out`}>
+            <div className={` bg-slate-800 flex flex-col justify-between z-50 fixed top-0 left-0 h-screen w-[90%] sm:w-[38%] ${menuIcon? "translate-x-0": "-translate-x-full"} transition-transform duration-1000 ease-in-out`}>
                     <span className='flex justify-end m-10 text-3xl text-right z-2 cursor-pointer' onClick={toggleMenuIcon}><FaTimes /></span>
                 <div className='text-white font-bold text-lg flex flex-col px-5 gap-5 py-5'>
                     <h3>Get in Touch</h3>
